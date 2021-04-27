f055ed0f02fd93ba2d7a506de593c5e44642af26
lyihongl <yihongliu00@gmail.com>
2021-04-26 21:07:08 -0600

    initial commit, setting up backend

---
+++ backend/src/index.ts
@@ -0,0 +1,6 @@
+import { MikroORM } from "@mikro-orm/core";
+import mikroConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+};
---
+++ backend/src/mikro-orm.config.ts
@@ -0,0 +1,16 @@
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+import { __prod__ } from "./constants";
+
+export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"), 
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  entities: [],
+  dbName: "test",
+  type: "postgresql",
+  user: "postgres",
+  password: "postgres",
+  debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
